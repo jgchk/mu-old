354aacb3425e93c0b479754be90c9c0738dc2eb0
jake cheek <jake@f-m.fm>
2020-06-26 17:52:11 -0400

    👷 use build instead of dist

--- packages/server/dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-var _a
-Object.defineProperty(exports, '__esModule', { value: true })
-const express_1 = require('express')
-const app = express_1.default()
-const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : '3000'
-app.get('/', (req, res) => res.send('Hello, world!'))
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
-//# sourceMappingURL=index.js.map
