36f087d1bac7faf5e9109a53ea216e73615f8d41
jake cheek <jake@f-m.fm>
2020-06-26 19:20:58 -0400

    📦 use koa instead of express

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,8 +1,9 @@
-import * as express from 'express'
+import * as Koa from 'koa'
+import * as koaStatic from 'koa-static'
 
-const app = express()
+const app = new Koa()
 const port: string | number = Number.parseInt(process.env.PORT) || 3000
 
-app.get('/', (req, res) => res.send('Hello, world!'))
+app.use(koaStatic('../client/build'))
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
