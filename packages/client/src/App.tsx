b1045c51ee3a598187c613b02626f8a8582cfdb3
jake cheek <jake@f-m.fm>
2020-06-26 19:58:20 -0400

    🏗 set up redux

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react'
 import { FC } from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
 
-const App: FC = () => <div>Hello, world!</div>
+const App: FC = () => (
+  <Provider store={store}>
+    <div>Hello, world!</div>
+  </Provider>
+)
 
 export default App
