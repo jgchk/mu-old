ec8bd36a3773933d7af93aa36466d06a0d76a5c2
jake cheek <jake@f-m.fm>
2020-07-02 12:48:40 -0400

    ✨ persist redux state

--- packages/client/src/store.ts
+++ packages/client/src/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { useDispatch } from 'react-redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import rootReducer from './modules'
 
+const persistedReducer = persistReducer({ key: 'root', storage }, rootReducer)
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 })
 
+export const persistor = persistStore(store)
 export default store
 
 export type AppDispatch = typeof store.dispatch
