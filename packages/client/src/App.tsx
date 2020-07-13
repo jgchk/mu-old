8a1e516257a576d9419485169c9abec0f7066fdb
jake cheek <jake@f-m.fm>
2020-07-12 21:30:08 -0400

    🔥 temporarily remove state persistence

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,20 +2,19 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import * as React from 'react'
 import { FC } from 'react'
 import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import apollo from './apollo'
 import Router from './components/Router'
 import { ThemeWrapper } from './kit/ThemeWrapper'
-import store, { persistor } from './store'
+import store from './store'
 
 const App: FC = () => (
   <ApolloProvider client={apollo}>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <ThemeWrapper>
-          <Router />
-        </ThemeWrapper>
-      </PersistGate>
+      {/* <PersistGate persistor={persistor}> */}
+      <ThemeWrapper>
+        <Router />
+      </ThemeWrapper>
+      {/* </PersistGate> */}
     </Provider>
   </ApolloProvider>
 )
