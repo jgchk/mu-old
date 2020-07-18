c6727a91d5273a88e13269188ab71969964d4140
jake cheek <jake@f-m.fm>
2020-07-18 12:42:56 -0400

    🚧 reenable redux persistence

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,19 +2,20 @@ import { ApolloProvider } from '@apollo/react-hooks'
 import * as React from 'react'
 import { FC } from 'react'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import apollo from './apollo'
 import Router from './components/Router'
 import { ThemeWrapper } from './kit/ThemeWrapper'
-import store from './store'
+import store, { persistor } from './store'
 
 const App: FC = () => (
   <ApolloProvider client={apollo}>
     <Provider store={store}>
-      {/* <PersistGate persistor={persistor}> */}
-      <ThemeWrapper>
-        <Router />
-      </ThemeWrapper>
-      {/* </PersistGate> */}
+      <PersistGate persistor={persistor}>
+        <ThemeWrapper>
+          <Router />
+        </ThemeWrapper>
+      </PersistGate>
     </Provider>
   </ApolloProvider>
 )
